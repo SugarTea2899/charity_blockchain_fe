dc23495467dcaae4f420990d1c9ae34f247daf44
doquangthinh99 <quangthinh123321@gmail.com>
2021-06-18 20:46:30 +0700

    add user projects pages

---
+++ app/containers/UserProjectPage/index.js
@@ -0,0 +1,42 @@
+import React, { memo } from 'react';
+import { makeStyles } from '@material-ui/core';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import MyTable from '../../components/MyTable';
+import MyAppBar from '../../components/MyAppBar';
+
+export const UserProjectPage = () => {
+  const classes = useStyle();
+  return (
+    <div className={classes.container}>
+      <MyAppBar />
+      <MyTable />
+    </div>
+  );
+};
+
+const useStyle = makeStyles({
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  logo: {
+    width: '45%',
+    padding: '5% 0% 8% 0%',
+  },
+});
+
+const mapStateToProps = createStructuredSelector({});
+
+const mapDispatchToProps = dispatch => {return {}};
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+  memo,
+)(UserProjectPage);
